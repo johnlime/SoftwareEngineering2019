fd5922a7bb7a7ab8b14c50b507e21fa01730bcb7
johnlime <thelimejuiceproductions@gmail.com>
2019-10-25 03:56:40 +0900

    added bptree leaf split functionality

--- 1018-bptree-leaf_split/bptree.cc
+++ 1018-bptree-leaf_split/bptree.cc
@@ -6,21 +6,20 @@ void
 print_temp(TEMP t)
 {
 	int i;
-
 	for (i = 0; i < t.nkey; i++) {
-		printf("[%p]", t.chi[i]);		
+		printf("[%p]", t.chi[i]);
 		printf("%d", t.key[i]);
 	}
-	printf("[%p]\n", t.chi[i]);		
+	printf("[%p]\n", t.chi[i]);
 }
 
 void
 print_tree_core(NODE *n)
 {
-	printf("["); 
+	printf("[");
 	for (int i = 0; i < n->nkey; i++) {
-		if (!n->isLeaf) print_tree_core(n->chi[i]); 
-		printf("%d", n->key[i]); 
+		if (!n->isLeaf) print_tree_core(n->chi[i]);
+		printf("%d", n->key[i]);
 		if (i != n->nkey-1 && n->isLeaf) putchar(' ');
 	}
 	if (!n->isLeaf) print_tree_core(n->chi[n->nkey]);
@@ -79,7 +78,7 @@ insert_in_leaf(NODE *leaf, int key, DATA *data)
 		for (i = leaf->nkey; i > 0; i--) {
 			leaf->chi[i] = leaf->chi[i-1] ;
 			leaf->key[i] = leaf->key[i-1] ;
-		} 
+		}
 		leaf->key[0] = key;
 		leaf->chi[0] = (NODE *)data;
 	}
@@ -90,10 +89,10 @@ insert_in_leaf(NODE *leaf, int key, DATA *data)
 		}
 
     // Step 2: Shift and insert
-		for (int j = leaf->nkey; j > i; j--) {		
+		for (int j = leaf->nkey; j > i; j--) {
 			leaf->chi[j] = leaf->chi[j-1] ;
 			leaf->key[j] = leaf->key[j-1] ;
-		} 
+		}
 		leaf->key[i] = key;
 		leaf->chi[i] = (NODE *)data;
 	}
@@ -106,44 +105,88 @@ void
 copy_from_left_to_temp(TEMP *temp, NODE *left)
 {
   // Step 1
+  for (int i = 0; i < N - 1; i++) {
+    temp->chi[i] = left->chi[i];
+    temp->key[i] = left->key[i];
+  }
+  temp->nkey = N - 1;
 }
 
 void
 insert_in_temp(TEMP *temp, int key, void *ptr)
 {
   // Step 2
+  int i;
+  if (key < temp->key[0]) {
+    for (i = temp->nkey; i > 0; i--) {
+      temp->chi[i] = temp->chi[i-1] ;
+      temp->key[i] = temp->key[i-1] ;
+    }
+    temp->key[0] = key;
+    temp->chi[0] = (NODE *)ptr;
+  }
+  else {
+  // Find the place to insert
+    for (i = 0; i < temp->nkey; i++) {
+      if (key < temp->key[i]) break;
+    }
+
+  // Shift and insert
+  for (int j = temp->nkey; j > i; j--) {
+    temp->chi[j] = temp->chi[j-1] ;
+    temp->key[j] = temp->key[j-1] ;
+  }
+  temp->key[i] = key;
+  temp->chi[i] = (NODE *)ptr;
+  }
+  temp->nkey++;
 }
 
 void
 erase_entries(NODE *node)
 {
   // Step 4
+  for (int i = 0; i < N - 1; i++) {
+    node->chi[i] = NULL;
+    node->key[i] = -1;
+  }
+  node->nkey -= N - 1;
 }
 
 void
 copy_from_temp_to_left(TEMP temp, NODE *left)
 {
   // Step 5
+  for (int i = 0; i < N/2; i++) {
+    left->chi[i] = temp.chi[i];
+    left->key[i] = temp.key[i];
+  }
+  left->nkey = N/2;
 }
 
 void
 copy_from_temp_to_right(TEMP temp, NODE *right)
 {
   // Step 6
+  for (int i = 0; i < N/2; i++) {
+    right->chi[i] = temp.chi[N/2 + i];
+    right->key[i] = temp.key[N/2 + i];
+  }
+  right->nkey = N/2;
 }
 
 void
 insert_in_parent(NODE *left_child, int rs_key, NODE *right_child)
 {
-  // Step 8 
+  // Step 8
   // You do not need to implement today.
   // This is for the next week.
 
-  // printf("Left:  "); print_tree(left_child);
-  // printf("Right: "); print_tree(right_child);
+  printf("Left:  "); print_tree(left_child);
+  printf("Right: "); print_tree(right_child);
 }
 
-void 
+void
 insert(int key, DATA *data)
 {
 	NODE *leaf;
@@ -166,7 +209,7 @@ insert(int key, DATA *data)
 
 		copy_from_left_to_temp(&temp, left);   // 0
 		insert_in_temp(&temp, key, data);      // 1
-    //print_temp(temp);
+    print_temp(temp);
 		right->chi[N-1] = left->chi[N-1];	     // 2
 		left->chi[N-1] = right;                // 3
 		erase_entries(left);                   // 4
@@ -196,7 +239,7 @@ search_core(const int key)
 }
 */
 
-int 
+int
 interactive()
 {
   int key;
